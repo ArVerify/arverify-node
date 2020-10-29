673f589e06387dc7a532ca8c0a2823446f63fa15
John Letey <johnletey@gmail.com>
2020-10-29 14:34:42 +0000

    feat: add verify endpoint

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,22 @@ router.get("/ping", async (ctx, next) => {
   await next();
 });
 
+router.get("/verify", async (ctx, next) => {
+  const addr = ctx.query["address"];
+
+  if (!addr) {
+    ctx.status = 400;
+    ctx.body = {
+      status: "error",
+      message: "address is required",
+    };
+  } else {
+    console.log(addr);
+  }
+
+  await next();
+});
+
 http.use(router.routes());
 
 http.listen(3000);
